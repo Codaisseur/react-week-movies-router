fed7f1806c8cfbc7f5028ae3f6aabe908c9aec7d
Domenico Gemoli <domenicogemoli@gmail.com>
2021-02-26 11:24:48 +0100

    Flesh out movie detail page

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,12 +1,56 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import "./MoviePage.scss";
+
 export default function MoviePage() {
-  const route_parameters = useParams();
-  console.log(route_parameters);
+  const { imdb_id } = useParams();
+
+  const [movieData, setMovieData] = useState();
+
+  console.log("render...", movieData);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        `https://omdbapi.com/?apikey=6a06f383&i=${imdb_id}`
+      ).then(r => r.json());
+
+      setMovieData(data);
+    }
+
+    fetchData();
+  }, [imdb_id]);
+
   return (
-    <div>
-      <p>Some specific movie here</p>
-      <p>{JSON.stringify(route_parameters)}</p>
+    <div className="MoviePage">
+      {movieData ? (
+        <>
+          <h2>{movieData.Title}</h2>
+          <div>
+            {movieData.Genre.split(", ").map((genre, index) => (
+              <span key={index} className="genre">
+                {genre}
+              </span>
+            ))}
+          </div>
+          <div className="details">
+            <img alt={movieData.Title} src={movieData.Poster} />
+            <div>
+              <h3>Director</h3>
+              <p>{movieData.Director}</p>
+              <h3>Language</h3>
+              <p>{movieData.Language}</p>
+              <h3>Plot</h3>
+              <p>{movieData.Plot}</p>
+              <h3>IMDB Rating</h3>
+              <p>{movieData.imdbRating}</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading....</p>
+      )}
     </div>
   );
 }
